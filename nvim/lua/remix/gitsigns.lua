73845a42affba93d6550f7f6559fe6aea41cddff
theRemix <jon@gomagames.com>
2023-12-14 17:21:57 -0800

    :fire: remove all old lvim config dirs

--- nvim/lua/remix/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-require('gitsigns').setup {
-    -- signs config
-	current_line_blame = true,
-	current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-	}
-}
